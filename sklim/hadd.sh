18dbdef45fdaf287478b937c1e2c9309bd9d1668
Andrea Malara <andrea.malara@cern.ch>
2017-06-06 09:23:08 +0200

    Preselection Selection

--- sklim/hadd.sh
+++ sklim/hadd.sh
@@ -1,21 +1,21 @@
+hadd -f PreSelected_RunB.root RunB/PreSelected_tree_*root
+hadd -f Histograms_PreSelected_RunB.root RunB/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunBv2.root RunBv2/PreSelected_tree_*root
-hadd -f Histograms_RunBv2.root RunBv2/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunBv2.root RunBv2/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunC.root RunC/PreSelected_tree_*root
-hadd -f Histograms_RunC.root RunC/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunC.root RunC/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunD.root RunD/PreSelected_tree_*root
-hadd -f Histograms_RunD.root RunD/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunD.root RunD/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunE.root RunE/PreSelected_tree_*root
-hadd -f Histograms_RunE.root RunE/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunE.root RunE/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunF.root RunF/PreSelected_tree_*root
-hadd -f Histograms_RunF.root RunF/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunF.root RunF/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunG.root RunG/PreSelected_tree_*root
-hadd -f Histograms_RunG.root RunG/Histograms_tree_*root
-hadd -f PreSelected_RunBv3.root RunBv3/PreSelected_tree_*root
-hadd -f Histograms_RunBv3.root RunBv3/Histograms_tree_*root
-hadd -f PreSelected_RunHv1.root RunHv1/PreSelected_tree_*root
-hadd -f Histograms_RunHv1.root RunHv1/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunG.root RunG/Histograms_PreSelected_tree_*root
 hadd -f PreSelected_RunHv2.root RunHv2/PreSelected_tree_*root
-hadd -f Histograms_RunHv2.root RunHv2/Histograms_tree_*root
+hadd -f Histograms_PreSelected_RunHv2.root RunHv2/Histograms_PreSelected_tree_*root
+hadd -f PreSelected_RunHv3.root RunHv3/PreSelected_tree_*root
+hadd -f Histograms_PreSelected_RunHv3.root RunHv3/Histograms_PreSelected_tree_*root
 
 
 
---
+++ sklim/launch.sh
@@ -0,0 +1,10 @@
+
+source TE_runB.sh 1
+source TE_runB2.sh 1
+source TE_runC.sh 1
+source TE_runD.sh 1
+source TE_runE.sh 1
+source TE_runF.sh 1
+source TE_runG1.sh 1
+source TE_runHv2.sh 1
+source TE_runHv3.sh 1
