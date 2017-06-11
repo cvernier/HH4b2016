197a9222666f5ab2450a1aebc213d9fc9dcd3498
Andrea Malara <andrea.malara@cern.ch>
2017-06-11 18:42:28 +0200

    update

--- PreselectedWithRegressionDeepCSV/LMRSelection_chi2/fit_split.c
+++ PreselectedWithRegressionDeepCSV/LMRSelection_chi2/fit_split.c
@@ -5,8 +5,8 @@
     //gSystem->Load("../../PDFs/LogisticXChebychev3_cxx.so");
     gROOT->LoadMacro("BackgroundPrediction_Kinematic_Split.c");
     
-    BackgroundPrediction_Kinematic_Split(252, 330, 5, // range low
-                                         285, 624,    // range hi
+    BackgroundPrediction_Kinematic_Split(250, 330, 5, // range low
+                                         285, 625,    // range hi
                                          "h_mX_SB_kinFit", // histogram
                                          "lin",
                                          "../Histograms_LMR_BTagTotal.root",
--- test.c
+++ test.c
@@ -1,1 +1,1 @@
- { gSystem->Load("PDFs/ExpGaussExp_cxx.so"); gROOT->LoadMacro("Display_SignalFits.cc"); Display_SignalFits("PreselectedWithRegressionDeepCSV","reg","MMRSelection_chi2","PreselectedWithRegressionDeepCSV/MMRSelection_chi2/fit","Histograms_MMR_GluGluToBulkGravitonToHHTo4B_M-",900,"false"); }
+ { gSystem->Load("PDFs/ExpGaussExp_cxx.so"); gROOT->LoadMacro("Display_SignalFits.cc"); Display_SignalFits("PreselectedWithRegressionDeepCSV","reg","MMRSelection_chi2","PreselectedWithRegressionDeepCSV/MMRSelection_chi2/fit","Histograms_MMR_GluGluToBulkGravitonToHHTo4B_M-",1000,"false"); }
